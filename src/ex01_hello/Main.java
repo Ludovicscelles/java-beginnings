9a4d1395abe1ef4613379b9b8a3d63ec24c04a1e
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-02-03 11:02:17 +0100

    ex01(java): simplify comments

--- src/ex01_hello/Main.java
+++ src/ex01_hello/Main.java
@@ -1,14 +1,10 @@
-// package declaration
-// defining the package name for this Java file
 package ex01_hello;
 
-// class declaration
+// Entry point of the program
 public class Main {
-  // main method - entry point of the program
-  // method signature 
-  // defining the main method which takes an array of Strings as arguments
+  
   public static void main(String[] args) {
-    // printing multiple greetings in different languages to the console
+    // displaying "Hello World" in multiple languages
     System.out.println("Hello World");
     System.out.println("Bonjour le monde");
     System.out.println("Hallo Welt");
