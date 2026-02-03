6e9b25361cc60b5b4fd101fec1b7d3cdb95fccb4
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-02-03 09:20:44 +0100

    ex01(java): add multiple languages for the 'Hello World' example

--- src/ex01_hello/Main.java
+++ src/ex01_hello/Main.java
@@ -3,6 +3,15 @@ package ex01_hello;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
+    System.out.println("Bonjour le monde");
+    System.out.println("Hallo Welt");
+    System.out.println("Ciao mondo");
+    System.out.println("Hola Mundo");
+    System.out.println("こんにちは世界");
+    System.out.println("안녕하세요 세계");
+    System.out.println("مرحبا بالعالم");  
+    System.out.println("שלום עולם");
+
   }
   
 }
