ca5147172915648d208e64f9f31662aa4b99f782
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-02-03 11:08:53 +0100

    ex02(java): simplify comments

--- src/ex02_variables/Main.java
+++ src/ex02_variables/Main.java
@@ -1,21 +1,15 @@
-// package declaration
-// defining the package name for this Java file
 package ex02_variables;
 
-// class declaration
 public class Main {
-  // main method - entry point of the program
-  // method signature
-  // defining the main method which takes an array of Strings as arguments
+
   public static void main(String[] args) {
-    // variable declarations and initializations - string, integer, and double types
     String firstname = "Ludovic";
     int age = 30;
     double height = 1.80;
 
-    // printing formatted output to the console
+    // simple example of variable usage 
     System.out.println("Je m'appelle " + firstname + ", j'ai " + age + " ans et je mesure " + String.format("%.2f", height) + " m.");
     System.out.println("Dans 10 ans, j'aurais " + (age + 10) + " ans.");
   
-}
+  }
 }
