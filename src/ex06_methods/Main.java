d6a260851e46f6e1547aa18a946a9f8dcebbfa20
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-02-03 13:58:01 +0100

    ex06_methods(java): add two methods and display sentence for each method

--- src/ex06_methods/Main.java
+++ src/ex06_methods/Main.java
@@ -5,23 +5,31 @@ public class Main {
   public static void main(String[] args) {
 
     // testing the methods
-    System.out.println(addition(3, 5));
-    System.out.println(max(48, 22));
-    System.out.println(isEven(22));
+    System.out.println("Le résultat de l'addition est de " + addition(3, 5));
+    System.out.println("Le résultat de la multiplication est de " + multiplication(4, 6));
+    System.out.println("Le résultat de la multiplication de trois nombres est de " + multiplyThree(2, 3, 4));
+    System.out.println("Le plus grand nombre est " + max(10, 20));
+    System.out.println("Le nombre 7 est-il pair ? " + isEven(7));
+    System.out.println("Le nombre 8 est-il pair ? " + isEven(8));
   }
 
-  // method to add two integers
   static int addition(int a, int b) {
       return a + b;
   }
 
-  // method to find the maximum of two integers
+  static int multiplication(int a, int b) {
+    return a * b;
+  }
+
+  static int multiplyThree(int a, int b, int c) {
+    return a * b * c;
+  }
+
   static int max(int a, int b) {
     return a > b ? a : b ; 
   }
 
 
-  // method to check if a number is even
   static boolean isEven(int n){
     return n % 2 == 0;
   }
