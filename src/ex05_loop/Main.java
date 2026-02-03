7486be7db345755e33b12a3e6e126ad99edc4775
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-02-03 11:59:14 +0100

    ex05_loop(java): add two for loops

---
+++ src/ex05_loop/Main.java
@@ -0,0 +1,18 @@
+package ex05_loop;
+
+public class Main {
+
+  public static void main (String[] args) {
+    // loop to print numbers from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
+
+    // loop to print multiplication table of 7
+    int n = 7;
+    for (int i = 1; i <= 10; i++ ){
+      System.out.println(n + " x " + i + " = " + (n * i));
+    }
+
+  }
+}
