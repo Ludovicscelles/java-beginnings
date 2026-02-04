fb6fbcd24d245045eb44d832a041a38c012cdbca
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-02-04 10:07:02 +0100

    ex07_arrays(java): use Math.max and printf

--- src/ex07_arrays/Main.java
+++ src/ex07_arrays/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
    public static void main(String[] args) {
     // declare and initialize an array of integers
-    int [] numbers = {12, 15, 9, 18, 14, 21};
+    int[] numbers = {12, 15, 9, 18, 14, 21};
 
     int sum = 0;
     int max = numbers[0];
@@ -12,19 +12,17 @@ public class Main {
     for (int number : numbers) {
       System.out.println(number);
       sum += number;
-      if (number > max) {
-        max = number;
-      }
+      max = Math.max(max, number);
     }
 
-    System.out.println("La Somme de ces nombres est de : " + sum);
+    System.out.printf("La Somme de ces nombres est de %d.%n", sum);
 
     // calculate the average
     // double is used to get a precise result
     double average = (double) sum / numbers.length;
 
-    System.out.println("La Moyenne de ces nombres est de : " + String.format("%.3f", average));
+    System.out.printf("La Moyenne de ces nombres est de %.3f.%n", average);
 
-    System.out.println("Le plus grand nombre est de : " + max);
+    System.out.printf("Le plus grand nombre est de %d.%n", max);
   }
 }
